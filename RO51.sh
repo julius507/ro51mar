7003eac12ab540dcafe826ca1d61af5a5e02b1e7
julius507 <terencesimega507@gmail.com>
2022-03-24 17:17:23 +0800

    first commit

---
+++ RO51.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "Unlocking Device..."
+ubus call version set_sn_imei_info '{"imei":"0"}' > /dev/null 2&>1
+sleep 1
+echo "Disabling OTA Updates..."
+sleep 1
+echo "Installing Permanent Band Locking Features..."
+wget https://github.com/marukoy/R051/raw/main/R051.bin -O /tmp/firmware.bin
+mtd -r write /tmp/firmware.bin /dev/mtd5 > /dev/null 2&>1
